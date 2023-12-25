bbad03a3ab7122a9e75f0ae4a273f5ef07ad9bdc
JoacoPuente <jpuente402@gmail.com>
2023-12-25 04:49:48 -0300

    Guardado 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Home } from "./screens/Home";
+import { Home } from "../src/screens/Home";
 import { About } from "./screens/about";
 import { Contact } from "./screens/contact";
 import { Products } from "./screens/products";
