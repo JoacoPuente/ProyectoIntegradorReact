01bba7e65948450ff0dae2835d57bdaa16f20e69
JoacoPuente <jpuente402@gmail.com>
2024-01-06 19:00:57 -0300

    g9

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+import React from "react";
 import "./App.css";
-import { Home } from "../src/screens/Home";
+import Home from "../src/screens/Home";
 import { About } from "./screens/about";
 import { Contact } from "./screens/contact";
 import { Products } from "./screens/products";
@@ -7,8 +8,7 @@ import { Products } from "./screens/products";
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <About />
+      <Home />
     </>
   );
 }
