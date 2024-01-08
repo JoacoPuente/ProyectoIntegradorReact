06846d928c22f6bf4338f14addf8d0d106fdedff
JoacoPuente <jpuente402@gmail.com>
2024-01-08 14:55:01 -0300

    g13

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
-// import { Home } from "../src/screens/Home";
 import { About } from "./screens/about";
 import { Contact } from "./screens/contact";
 import { Products } from "./screens/products";
 
 function App() {
-  return <>{/* <Home /> */}</>;
+  return (
+    <>
+      <About />
+    </>
+  );
 }
 
 export default App;
