46f48f66ccb888c74b562fcb868f77def3a5ab62
JoacoPuente <jpuente402@gmail.com>
2024-01-08 14:44:58 -0300

    g10

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Home from "../src/screens/Home";
+import { Home } from "../src/screens/Home";
 import { About } from "./screens/about";
 import { Contact } from "./screens/contact";
 import { Products } from "./screens/products";
--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { HeaderComponent } from "../components/header/HeaderComponent";
+// import { HeaderComponent } from "../components/header/HeaderComponent";
 import { HeroComponent } from "../components/hero/HeroComponent";
 import { FooterComponent } from "../components/footer/FooterComponent";
 
 export const Home = () => {
   return (
     <div>
-      <HeaderComponent />
+      {/* <HeaderComponent /> */}
       <HeroComponent />
       <FooterComponent />
     </div>
