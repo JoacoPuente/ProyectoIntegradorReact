3b24e6dd3a0c5a9294682920a3bb3210d3c3e192
JoacoPuente <jpuente402@gmail.com>
2024-01-08 14:47:42 -0300

    g11

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 // import { HeaderComponent } from "../components/header/HeaderComponent";
-import { HeroComponent } from "../components/hero/HeroComponent";
-import { FooterComponent } from "../components/footer/FooterComponent";
+// import { HeroComponent } from "../components/hero/HeroComponent";
+// import { FooterComponent } from "../components/footer/FooterComponent";
 
 export const Home = () => {
   return (
     <div>
       {/* <HeaderComponent /> */}
-      <HeroComponent />
-      <FooterComponent />
+      {/* <HeroComponent /> */}
+      {/* <FooterComponent /> */}
     </div>
   );
 };
