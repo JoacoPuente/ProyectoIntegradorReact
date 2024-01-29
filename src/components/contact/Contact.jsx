18c14238ccbe32ba95ca9d0d03f0bdb1a83ac567
JoacoPuente <jpuente402@gmail.com>
2024-01-28 23:07:32 -0300

    contacto

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,93 +1,122 @@
 import React from "react";
-import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Switch } from "@headlessui/react";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export const Contact = () => {
-  const [agreed, setAgreed] = useState(false);
+  const validationSchema = Yup.object({
+    firstname: Yup.string()
+      .matches(/^[a-zA-Z]+$/, "Ingrese un nombre válido (solo letras)")
+      .required("Ingrese un nombre válido"),
+    lastname: Yup.string()
+      .matches(/^[a-zA-Z]+$/, "Ingrese un apellido válido (solo letras)")
+      .required("Ingrese un apellido válido"),
+    email: Yup.string()
+      .email("Ingrese un correo electrónico válido")
+      .required("Ingrese un correo electrónico"),
+    phonenumber: Yup.string()
+      .matches(/^[0-9]+$/, "Ingrese un número de teléfono válido")
+      .required("Ingrese un número de teléfono"),
+    messaje: Yup.string().required("Ingrese un mensaje válido"),
+  });
+  const {
+    values,
+    handleChange,
+    getFieldProps,
+    handleSubmit,
+    resetForm,
+    errors,
+    touched,
+  } = useFormik({
+    validationSchema: validationSchema,
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      phonenumber: "",
+      messaje: "",
+    },
+    onSubmit: async (values) => {
+      const isValid = await validationSchema.isValid(values);
+
+      if (isValid) {
+        alert("Mensaje enviado");
+
+        resetForm();
+      } else {
+        alert("Revise los datos antes de enviar");
+      }
+    },
+  });
 
   return (
-    <div className="px-6 py-24 bg-white isolate sm:py-32 lg:px-8">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
+    <div className="px-6 py-24 isolate sm:py-32 lg:px-8 bg-waikawa-gray-100">
       <div className="max-w-2xl mx-auto text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Contact sales
+        <h2 className="text-3xl font-bold tracking-tight text-waikawa-gray-500 sm:text-4xl">
+          Contacta Con Nosotros
         </h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
+        <p className="mt-2 text-lg leading-8 text-waikawa-gray-500">
+          Consulta, opina y conecta.Estamos listos para ayudarte.
         </p>
       </div>
-      <form
-        action="#"
-        method="POST"
-        className="max-w-xl mx-auto mt-16 sm:mt-20"
-      >
+      <form onSubmit={handleSubmit} className="max-w-xl mx-auto mt-16 sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label
               htmlFor="first-name"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
-              First name
+              Nombre
             </label>
             <div className="mt-2.5">
               <input
                 type="text"
                 name="first-name"
                 id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                {...getFieldProps("firstname")}
+                className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-2 ring-inset
+                ${
+                  touched.firstname
+                    ? errors.firstname
+                      ? "ring-red-500 "
+                      : "ring-green-500 "
+                    : "ring-gray-300"
+                } placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-waikawa-gray-800 sm:text-sm sm:leading-6 `}
               />
             </div>
+            {touched.firstname && errors.firstname && (
+              <p className="mt-2 text-sm font-medium text-red-500">
+                {errors.firstname}
+              </p>
+            )}
           </div>
           <div>
             <label
               htmlFor="last-name"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
-              Last name
+              Apellido
             </label>
             <div className="mt-2.5">
               <input
                 type="text"
                 name="last-name"
                 id="last-name"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                {...getFieldProps("lastname")}
+                className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-2 ring-inset
+                ${
+                  touched.lastname
+                    ? errors.lastname
+                      ? "ring-red-500 "
+                      : "ring-green-500 "
+                    : "ring-gray-300"
+                } placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-waikawa-gray-800 sm:text-sm sm:leading-6 `}
               />
             </div>
-          </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="company"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Company
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="company"
-                id="company"
-                autoComplete="organization"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            {touched.lastname && errors.lastname && (
+              <p className="mt-2 text-sm font-medium text-red-500">
+                {errors.lastname}
+              </p>
+            )}
           </div>
           <div className="sm:col-span-2">
             <label
@@ -101,98 +130,88 @@ export const Contact = () => {
                 type="email"
                 name="email"
                 id="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                {...getFieldProps("email")}
+                className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-2 ring-inset
+                ${
+                  touched.email
+                    ? errors.email
+                      ? "ring-red-500 "
+                      : "ring-green-500 "
+                    : "ring-gray-300"
+                } placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-waikawa-gray-800 sm:text-sm sm:leading-6 `}
               />
             </div>
+            {touched.email && errors.email && (
+              <p className="mt-2 text-sm font-medium text-red-500">
+                {errors.email}
+              </p>
+            )}
           </div>
           <div className="sm:col-span-2">
             <label
               htmlFor="phone-number"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
-              Phone number
+              Número de Telefono
             </label>
             <div className="relative mt-2.5">
-              <div className="absolute inset-y-0 left-0 flex items-center">
-                <label htmlFor="country" className="sr-only">
-                  Country
-                </label>
-                <select
-                  id="country"
-                  name="country"
-                  className="h-full py-0 pl-4 text-gray-400 bg-transparent border-0 rounded-md bg-none pr-9 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
-                >
-                  <option>US</option>
-                  <option>CA</option>
-                  <option>EU</option>
-                </select>
-                <ChevronDownIcon
-                  className="absolute top-0 w-5 h-full text-gray-400 pointer-events-none right-3"
-                  aria-hidden="true"
-                />
-              </div>
               <input
                 type="tel"
                 name="phone-number"
                 id="phone-number"
-                autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                {...getFieldProps("phonenumber")}
+                className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-2 ring-inset
+                ${
+                  touched.phonenumber
+                    ? errors.phonenumber
+                      ? "ring-red-500 "
+                      : "ring-green-500 "
+                    : "ring-gray-300"
+                } placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-waikawa-gray-800 sm:text-sm sm:leading-6 `}
               />
             </div>
+            {touched.phonenumber && errors.phonenumber && (
+              <p className="mt-2 text-sm font-medium text-red-500">
+                {errors.phonenumber}
+              </p>
+            )}
           </div>
           <div className="sm:col-span-2">
             <label
               htmlFor="message"
               className="block text-sm font-semibold leading-6 text-gray-900"
             >
-              Message
+              Mensaje
             </label>
             <div className="mt-2.5">
               <textarea
                 name="message"
                 id="message"
+                {...getFieldProps("messaje")}
                 rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
+                className={`block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-2 ring-inset
+                ${
+                  touched.messaje
+                    ? errors.messaje
+                      ? "ring-red-500 "
+                      : "ring-green-500 "
+                    : "ring-gray-300"
+                } placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-waikawa-gray-800 sm:text-sm sm:leading-6 `}
               />
             </div>
+            {touched.messaje && errors.messaje && (
+              <p className="mt-2 text-sm font-medium text-red-500">
+                {errors.messaje}
+              </p>
+            )}
           </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex items-center h-6">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className={classNames(
-                  agreed ? "bg-indigo-600" : "bg-gray-200",
-                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                )}
-              >
-                <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    agreed ? "translate-x-3.5" : "translate-x-0",
-                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                  )}
-                />
-              </Switch>
-            </div>
-            <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                privacy&nbsp;policy
-              </a>
-              .
-            </Switch.Label>
-          </Switch.Group>
         </div>
-        <div className="mt-10">
+        <div className="mt-10 ">
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="block w-full rounded-md bg-waikawa-gray-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-waikawa-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-waikawa-gray-500"
           >
-            Let's talk
+            Enviar
           </button>
         </div>
       </form>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,14 +4,14 @@ import { Hero } from "../components/hero/Hero";
 import { Footer } from "../components/footer/Footer";
 import { ProductWidget } from "../components/products/productsWidget/ProductWidget";
 import { CategoryWidget } from "../components/categories/categoryWidget/CategoryWidget";
+import { Contact } from "../components/contact/Contact";
 
 export const Home = () => {
   return (
     <div>
       <Header />
       <Hero />
-      <CategoryWidget />
-      <ProductWidget />
+      <Contact />
       <Footer />
     </div>
   );
